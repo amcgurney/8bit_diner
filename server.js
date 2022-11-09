f2db3a201dd5baa79226bd9694619cdfc10f6d3d
Alan McGurney <mcgurneyalan@gmail.com>
2022-11-09 14:17:50 -0500

    smooth sailing

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
 let Open = document.getElementById("neon")
 Open.onclick = turnOn
 
-let Close = document.getElementById("neon")
-Close.onclick = turnOff
-
-
-function turnOff () {
-  Open.style.color = "black"
-  Open.style.animation = "none"
-}
-
-function turnOn () {
-  Open.style.color = "#AB10B2;"
-  Open.style.animation = "none"
-}
